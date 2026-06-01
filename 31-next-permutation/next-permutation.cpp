020adc2f4864d8d40b3d04e58569a3c092fe4d06
Raj jaiswal <rajjaiswal6394@gmail.com>
2026-06-01 18:27:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.19%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n= nums.size();
+
+        int pivot=-1;
+        for(int i= n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                pivot=i;
+                break;
+            }
+        }
+        if(pivot==-1){ reverse(nums.begin(),nums.end()); return;}
+        reverse(nums.begin()+pivot+1,nums.end());
+
+        int justGreater=-1;
+        for(int i=pivot+1; i<n; i++){
+            if(nums[i]>nums[pivot]){
+                justGreater=i;
+                break;
+            }
+        }
+        int temp=nums[pivot];
+        nums[pivot]=nums[justGreater];
+        nums[justGreater]=temp;
+
+        return; 
     }
 };
