a1f5aaa0fb7fee9bc6193f08cdc15b8e20b3be9e
Raj jaiswal <rajjaiswal6394@gmail.com>
2026-09-15 18:52:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.11%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0, one=0, two=nums.size()-1;
+
+        while(one<=two){
+
+           if(nums[one]==2){
+            swap(nums[one],nums[two]);
+            two--;
+           }
+
+           else if(nums[one]==0){
+            swap(nums[one],nums[zero]);
+            zero++;
+            one++;
+           }
+
+           else one++;
+
+            
+        } 
+       
+        
+    }
+};
